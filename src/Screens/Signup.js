859cdd786f859e368212911c5e217d68b4e9e25b
jvbrian <brianjarabav@hotmail.com>
2022-11-15 21:17:20 -0500

    link al repo

--- src/Screens/Signup.js
+++ src/Screens/Signup.js
@@ -23,8 +23,8 @@ function Copyright(props) {
       {...props}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/JVBrian">
+        JVBrian
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
